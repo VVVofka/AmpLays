f0aff6872a36d20ec93270e49d7b72e65591da6e
vvvofka@yandex.ru <vvvofka@yandex.ru>
2020-01-26 15:38:23 +0300

    vtype+

--- AmpLays/ProcA.h
+++ AmpLays/ProcA.h
@@ -3,18 +3,18 @@
 using namespace concurrency;
 class ProcA {
 private:
-    array_view<const float, 2> m_mA;
-    array_view<float, 2> m_mC;
+    array_view<const vtype, 2> m_mA;
+    array_view<vtype, 2> m_mC;
     int m_W;
 public:
-    ProcA(const array_view<const float, 2>& a,
-        const array_view<float, 2>& c,
+    ProcA(const array_view<const vtype, 2>& a,
+        const array_view<vtype, 2>& c,
         int w) : m_mA(a), 
         m_mC(c), 
         m_W(w) {}
     void operator()(index<2> idx) const restrict(amp) {
         int row = idx[0]; int col = idx[1];
-        float sum = 0.0f;
+        vtype sum = 0.0f;
         for(int i = 0; i < m_W; i++)
             sum += m_mA(row, i); 
         m_mC[idx] = sum;
